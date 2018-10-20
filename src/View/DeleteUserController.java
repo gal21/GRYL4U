aa907b6448f26cc00ff713c61d3c0eae2a2e037b
gal21 <gal.lahiani@gmail.com>
2018-10-20 14:43:54 +0300

    Merge branch 'master' of https://github.com/gal21/GRYL4U
    # Conflicts:
    #	src/View/UpdateUserController.java

--- src/View/DeleteUserController.java
+++ src/View/DeleteUserController.java
@@ -1,4 +1,29 @@
 package View;
 
-public class DeleteUserController {
+import javafx.scene.control.Alert;
+
+public class DeleteUserController extends UserController {
+    public javafx.scene.control.TextField userName;
+    public javafx.scene.control.TextField userPassword;
+    public javafx.scene.control.Button delete;
+    public javafx.scene.control.Button clear;
+
+
+    public void deleteUser() {
+        if (searchUserData(userName.getText()) != null)
+        {
+            deleteUser(); // of controller
+            Alert a = new Alert(Alert.AlertType.INFORMATION);
+            a.setContentText("The user is deleted.");
+            a.show();
+        }
+        else
+        {
+            Alert a = new Alert(Alert.AlertType.INFORMATION);
+            a.setContentText("You entered user that not exist, please enter again");
+            a.show();
+            return;
+        }
+        return;
+    }
 }
