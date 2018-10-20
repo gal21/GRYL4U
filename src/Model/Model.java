486eb41fca1e4417727968e10ead56a18646d5d9
RonElhar <ronelharrar@gmail.com>
2018-10-20 14:37:06 +0300

    Merge branch 'master' of https://github.com/gal21/GRYL4U
    # Conflicts:
    #	src/Model/User.java

---
+++ src/Model/Model.java
@@ -0,0 +1,21 @@
+package Model;
+
+public class Model {
+    private sql_create sql_db;
+
+    public Model(){
+        sql_db = new sql_create();
+    }
+
+    public void insertUser(User newUser){
+
+    }
+
+    public void updateUser(User updatedUser){
+
+    }
+
+    public User searchUserData(String userName) {
+        return null;
+    }
+}
